b01ee7d2061cfdf024f0de6c55d07e2266f0ccf8
RealVaporWaver <101952674+RealVaporWaver@users.noreply.github.com>
2022-09-28 19:39:28 +0000

    Add files via upload

---
+++ Weed/Weed/Pages/IndexComponent.cs
@@ -0,0 +1,33 @@
+﻿using Blazor.Extensions.Canvas.Canvas2D;
+using Blazor.Extensions;
+using Microsoft.JSInterop;
+
+namespace Weed.Pages
+{
+    public class IndexComponent
+    {
+        BECanvasComponent _canvasReference = null;
+        Canvas2DContext _outputCanvasContext;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (!firstRender)
+                return;
+            _outputCanvasContext = await _canvasReference.CreateCanvas2DAsync();
+
+            await JsRuntime.InvokeAsync<object>("initGame", DotNetObjectReference.Create(this));
+        }
+
+        [JSInvokable]
+        public async ValueTask GameLoop(float timeStamp)
+        {
+            await _outputCanvasContext.ClearRectAsync(0, 0, 300, 400);
+
+            await _outputCanvasContext.SetFillStyleAsync("green");
+            await _outputCanvasContext.FillRectAsync(10, 50, 300, 100);
+
+            await _outputCanvasContext.SetFontAsync("24px verdana");
+            await _outputCanvasContext.StrokeTextAsync($"time: {timeStamp}", 20, 80);
+        }
+    }
+}
